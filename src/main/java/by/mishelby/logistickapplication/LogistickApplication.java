42a4f2d1e7f1162af630602bdccda42281b13801
Misha <lorestatys@Yandex.ru>
2024-08-18 16:25:57 +0300

    Initial commit

---
+++ src/main/java/by/mishelby/logistickapplication/LogistickApplication.java
@@ -0,0 +1,13 @@
+package by.mishelby.logistickapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogistickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/mishelby/logistickapplication/LogistickApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mishelby.logistickapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogistickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
